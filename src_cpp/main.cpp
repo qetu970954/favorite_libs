3ea6ead1b3bc25047f44f945d1169e27daa7ac50
Tzu-Yee <fdja1974.cs07g@nctu.edu.tw>
2020-09-18 11:28:20 +0800

    Rearrange structure for the project

---
+++ src_cpp/main.cpp
@@ -0,0 +1,5 @@
+#include "3rd_party.hpp"
+
+int main() {
+  cout << absl::StrCat("Hello ", 2020, " World");
+}
---
+++ src_cpp/tests/test_sample.cc
@@ -0,0 +1,5 @@
+#include "3rd_party.hpp"
+
+TEST(ArithmeticTest, one_plus_one){
+  ASSERT_EQ(3, 3);
+}
